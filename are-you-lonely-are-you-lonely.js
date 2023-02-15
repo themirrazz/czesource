47975892cbb1b5a33f0376d48d0a04402750fe81
themirrazz <89490659+themirrazz@users.noreply.github.com>
2023-02-15 13:59:41 -0800

    Update are-you-lonely-are-you-lonely.js

--- are-you-lonely-are-you-lonely.js
+++ are-you-lonely-are-you-lonely.js
@@ -2,15 +2,15 @@ wsh.Run("C:\\Windows\\explorer T:\\users\\kekoads26.SSD88J\\Downloads\\source\\b
 Sleep(1000)
 ShowDialogCenterOffset("Warning","My",48,0,0)
 Sleep(1000)
-ShowDialogCenterOffset("Warning","My life",48,0,0)
+ShowDialogCenterOffset("Warning","My life",48,10,10)
 Sleep(1000)
-ShowDialogCenterOffset("Warning","My life is",48,0,0)
+ShowDialogCenterOffset("Warning","My life is",48,10,10)
 Sleep(1000)
-ShowDialogCenterOffset("Warning","My life is on",48,0,0)
+ShowDialogCenterOffset("Warning","My life is on",48,20,20)
 Sleep(1000)
-ShowDialogCenterOffset("Warning","My life is on the",48,0,0)
+ShowDialogCenterOffset("Warning","My life is on the",48,30,30)
 Sleep(1000)
-ShowDialogCenterOffset("Warning","My life is on the line",48,0,0)
+ShowDialogCenterOffset("Warning","My life is on the line",48,40,40)
 Sleep(1000)
 wsh.Run("taskkill /F /IM message-box-centerby.exe")
 Sleep(5000)
@@ -18,15 +18,15 @@ wsh.Run("C:\\Windows\\explorer T:\\users\\kekoads26.SSD88J\\Downloads\\source\\b
 Sleep(1000)
 ShowDialogCenterOffset("Warning","My",48,0,0)
 Sleep(1000)
-ShowDialogCenterOffset("Warning","My life",48,0,0)
+ShowDialogCenterOffset("Warning","My life",48,10,10)
 Sleep(1000)
-ShowDialogCenterOffset("Warning","My life is",48,0,0)
+ShowDialogCenterOffset("Warning","My life is",48,20,20)
 Sleep(1000)
-ShowDialogCenterOffset("Warning","My life is on",48,0,0)
+ShowDialogCenterOffset("Warning","My life is on",48,30,30)
 Sleep(1000)
-ShowDialogCenterOffset("Warning","My life is on the",48,0,0)
+ShowDialogCenterOffset("Warning","My life is on the",48,40,40)
 Sleep(1000)
-ShowDialogCenterOffset("Warning","My life is on the line",48,0,0)
+ShowDialogCenterOffset("Warning","My life is on the line",48,50,50)
 Sleep(1000)
 wsh.Exec("taskkill /F /IM message-box-centerby.exe")
 Sleep(5000)
@@ -43,24 +43,24 @@ ShowDialog(
   "Lonely",
   "Are You",
   16,
-  50,
-  50
+  70,
+  70
 )
 Sleep(1000)
 ShowDialog(
   "Lonely",
   "Are You Lone-",
   16,
-  50,
-  50
+  90,
+  90
 )
 Sleep(1000)
 ShowDialog(
   "Lonely",
   "Are You Lonely",
   16,
-  50,
-  50
+  110,
+  110
 )
 Sleep(1000)
 
@@ -68,32 +68,32 @@ ShowDialog(
   "Lonely",
   "Are",
   16,
-  50,
-  50
+  1000,
+  200
 )
 Sleep(1000)
 ShowDialog(
   "Lonely",
   "Are You",
   16,
-  50,
-  50
+  1000-20,
+  180
 )
 Sleep(1000)
 ShowDialog(
   "Lonely",
   "Are You Lone-",
   16,
-  50,
-  50
+  1000-40,
+  160
 )
 Sleep(1000)
 ShowDialog(
   "Lonely",
   "Are You Lonely",
   16,
-  50,
-  50
+  1000-60,
+  140
 )
 Sleep(1000)
 
