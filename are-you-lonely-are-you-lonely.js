2db85d21409904d46d6059a0fa0c396277dfd849
themirrazz <89490659+themirrazz@users.noreply.github.com>
2023-02-15 13:57:18 -0800

    Create are-you-lonely-are-you-lonely.js

---
+++ are-you-lonely-are-you-lonely.js
@@ -0,0 +1,195 @@
+wsh.Run("C:\\Windows\\explorer T:\\users\\kekoads26.SSD88J\\Downloads\\source\\broken.exe")
+Sleep(1000)
+ShowDialogCenterOffset("Warning","My",48,0,0)
+Sleep(1000)
+ShowDialogCenterOffset("Warning","My life",48,0,0)
+Sleep(1000)
+ShowDialogCenterOffset("Warning","My life is",48,0,0)
+Sleep(1000)
+ShowDialogCenterOffset("Warning","My life is on",48,0,0)
+Sleep(1000)
+ShowDialogCenterOffset("Warning","My life is on the",48,0,0)
+Sleep(1000)
+ShowDialogCenterOffset("Warning","My life is on the line",48,0,0)
+Sleep(1000)
+wsh.Run("taskkill /F /IM message-box-centerby.exe")
+Sleep(5000)
+wsh.Run("C:\\Windows\\explorer T:\\users\\kekoads26.SSD88J\\Downloads\\source\\broken.exe")
+Sleep(1000)
+ShowDialogCenterOffset("Warning","My",48,0,0)
+Sleep(1000)
+ShowDialogCenterOffset("Warning","My life",48,0,0)
+Sleep(1000)
+ShowDialogCenterOffset("Warning","My life is",48,0,0)
+Sleep(1000)
+ShowDialogCenterOffset("Warning","My life is on",48,0,0)
+Sleep(1000)
+ShowDialogCenterOffset("Warning","My life is on the",48,0,0)
+Sleep(1000)
+ShowDialogCenterOffset("Warning","My life is on the line",48,0,0)
+Sleep(1000)
+wsh.Exec("taskkill /F /IM message-box-centerby.exe")
+Sleep(5000)
+
+ShowDialog(
+  "Lonely",
+  "Are",
+  16,
+  50,
+  50
+)
+Sleep(1000)
+ShowDialog(
+  "Lonely",
+  "Are You",
+  16,
+  50,
+  50
+)
+Sleep(1000)
+ShowDialog(
+  "Lonely",
+  "Are You Lone-",
+  16,
+  50,
+  50
+)
+Sleep(1000)
+ShowDialog(
+  "Lonely",
+  "Are You Lonely",
+  16,
+  50,
+  50
+)
+Sleep(1000)
+
+ShowDialog(
+  "Lonely",
+  "Are",
+  16,
+  50,
+  50
+)
+Sleep(1000)
+ShowDialog(
+  "Lonely",
+  "Are You",
+  16,
+  50,
+  50
+)
+Sleep(1000)
+ShowDialog(
+  "Lonely",
+  "Are You Lone-",
+  16,
+  50,
+  50
+)
+Sleep(1000)
+ShowDialog(
+  "Lonely",
+  "Are You Lonely",
+  16,
+  50,
+  50
+)
+Sleep(1000)
+
+ShowDialogCenterOffset(
+  "Lonely",
+  "Are",
+  16,
+  0,
+  0
+)
+Sleep(1000)
+ShowDialogCenterOffset(
+  "Lonely",
+  "Are You",
+  16,
+  20,
+  20
+)
+Sleep(1000)
+ShowDialogCenterOffset(
+  "Lonely",
+  "Are You Lone-",
+  16,
+  40,
+  40
+)
+Sleep(1000)
+ShowDialogCenterOffset(
+  "Lonely",
+  "Are You Lonely",
+  16,
+  60,
+  60
+)
+Sleep(1000)
+ShowDialog(
+  "Lonely",
+  "I'll",
+  48,
+  Math.floor(Math.random()*1920),
+  Math.floor(Math.random()*1080)
+)
+Sleep(1000)
+ShowDialog(
+  "Lonely",
+  "I'll stand",
+  48,
+  Math.floor(Math.random()*1920),
+  Math.floor(Math.random()*1080)
+)
+Sleep(1000)
+ShowDialog(
+  "Lonely",
+  "I'll stand by",
+  48,
+  Math.floor(Math.random()*1920),
+  Math.floor(Math.random()*1080)
+)
+Sleep(1000)
+ShowDialog(
+  "Lonely",
+  "I'll stand by you",
+  48,
+  Math.floor(Math.random()*1920),
+  Math.floor(Math.random()*1080)
+)
+Sleep(1000)
+ShowDialog(
+  "Lonely",
+  "I'll",
+  48,
+  Math.floor(Math.random()*1920),
+  Math.floor(Math.random()*1080)
+)
+Sleep(1000)
+ShowDialog(
+  "Lonely",
+  "I'll stand",
+  48,
+  Math.floor(Math.random()*1920),
+  Math.floor(Math.random()*1080)
+)
+Sleep(1000)
+ShowDialog(
+  "Lonely",
+  "I'll stand by",
+  48,
+  Math.floor(Math.random()*1920),
+  Math.floor(Math.random()*1080)
+)
+Sleep(1000)
+ShowDialog(
+  "Lonely",
+  "I'll stand by you",
+  48,
+  Math.floor(Math.random()*1920),
+  Math.floor(Math.random()*1080)
+)
+Sleep(1000)
